edbacbd03986a9dcc59ca77ff8e269bb6991fc6b
Jason Torres <jmtorres@rescomp1.(none)>
2017-07-24 15:03:08 +0100

    updated directory paths, input_dir where needed

--- 01.0_build_fgwas_bedfile.py
+++ 01.0_build_fgwas_bedfile.py
@@ -11,10 +11,14 @@ import subprocess as sp
 # globals
 bedtools = "/apps/well/bedtools/2.24.0/bedtools" # intersect -a test.bed -b TFBS.bed -wb > mytest.bed"
 cur_dir = "path_to_current_directory/"
+input_dir = cur_dir + "fgwas_input/"
 fgwas_head_list = ["CHR","POS0","POS","SNPID","F","Z","PVAL","NCASE","NCONTROL"] # fgwas field names for gwas columns
-annot_bed_file = "path_to_bedfile.bed"
+annot_bed_file = input_dir + "path_to_bedfile.bed"
 gwas_bed_file = "path_to_gwas_bedfile.bed"
-outfile = "path_to_output_file.fgwas"
+outfile = input_dir + "path_to_output_file.fgwas"
+
+print "Be sure to load R module before running script: module load R/3.3.1"
+
 
 # functions
 
@@ -49,14 +53,14 @@ def config_dic(bed_file):
 
 def bed_intersect(gwas_bed,annot_bed):
     sys.stdout.write("\nBedtools intersect...\n")
-    command_list1 = ["cat",gwas_bed,"|","cut","-f","1,2,3,4",">",cur_dir+"temp.bed"]
+    command_list1 = ["cat",gwas_bed,"|","cut","-f","1,2,3,4",">",input_dir+"temp.bed"]
     sp.check_call(" ".join(command_list1),shell=True)
-    command_list2 = [bedtools, "intersect", "-a", cur_dir+"temp.bed", "-b", annot_bed,
-                    "-wb", ">", cur_dir+"intersect.temp"]
+    command_list2 = [bedtools, "intersect", "-a", input_dir+"temp.bed", "-b", annot_bed,
+                    "-wb", ">", input_dir+"intersect.temp"]
     sp.check_call(" ".join(command_list2),shell=True)
 
 def build_dic(dic):
-    fin = open(cur_dir+"intersect.temp",'r')
+    fin = open(input_dir+"intersect.temp",'r')
     for line in fin:
         l = line.strip().split()
         chrom,pos1,pos2,annot = l[0],l[1],l[2],l[7]
@@ -71,7 +75,7 @@ def build_matrix_file(gwas_bed, annot_list, dic, fgwas_head_list):
     '''
     sys.stdout.write("\nWriting Matrix file\n")
     fin = open(gwas_bed,'r')
-    fout = open(cur_dir+"intermediate.fgwas",'w')
+    fout = open(input_dir+"intermediate.fgwas",'w')
     head_list = fgwas_head_list + annot_list
     fout.write("\t".join(head_list)+"\n")
     count = 0
@@ -90,7 +94,7 @@ def build_matrix_file(gwas_bed, annot_list, dic, fgwas_head_list):
         fout.write("\t".join(l)+"\n")
     fin.close()
     fout.close()
-    command_list = ["gzip", cur_dir+"intermediate.fgwas"]
+    command_list = ["gzip", input_dir+"intermediate.fgwas"]
     sys.stdout.write("\nCompressing file...\n")
     sp.check_call(" ".join(command_list),shell=True)
 
@@ -98,7 +102,7 @@ def process_file(infile,outfile):
     '''
     Will use R to sort, remove duplicate SNPIDS, and determine distance to TSS
     '''
-    r_file = cur_dir+"process.R"
+    r_file = cur_dir+"01.process.R"
     fout=open(r_file,'w')
     script='''
 library("data.table")
@@ -138,8 +142,6 @@ write.table(df,"%s",sep="\t",row.names=F,quote=F)
     ''' % (cur_dir, infile, outfile)
     fout.write(script)
     fout.close()
-    call = ["module","load","R/3.3.1"]
-    sp.check_call(" ".join(call),shell=True)
     call = ["Rscript","--vanilla",r_file]
     sp.check_call(" ".join(call),shell=True)
     command_list = ["gzip", outfile]
@@ -152,6 +154,6 @@ def main():
     mylist, mydic = config_dic(annot_bed_file)
     mydic = build_dic(mydic)
     build_matrix_file(gwas_bed_file,mylist,mydic,fgwas_head_list)
-    process_file(cur_dir+"intermediate.fgwas.gz",outfile)
+    process_file(input_dir+"intermediate.fgwas.gz",outfile)
 
 if (__name__=="__main__"): main()
